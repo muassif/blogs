72f27cc93dea99daf03953b6790954e27253edbe
muassif <32828975+muassif@users.noreply.github.com>
2021-07-16 15:18:54 +0400

    Create main.py

---
+++ gcf_pubsub/main.py
@@ -0,0 +1,11 @@
+import base64
+
+def process_pubsub_event(event, context):
+
+    message = base64.b64decode(event['data']).decode('utf-8')
+
+    event_id = context.event_id
+    event_type = context.event_type
+
+    print(f"A new event is received: id={event_id}, type={event_type}")
+    print(f"data = {message}")
